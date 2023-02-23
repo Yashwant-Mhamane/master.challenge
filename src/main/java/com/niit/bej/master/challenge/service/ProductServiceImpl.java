78b3b06900540f04b756df5da650756e9d6e006d
yashwantmhamane <yashwantmhamane@gmail.com>
2023-02-23 16:52:02 +0530

    ProductServiceImpl.java class created it implent ProductService interface

---
+++ src/main/java/com/niit/bej/master/challenge/service/ProductServiceImpl.java
@@ -0,0 +1,17 @@
+package com.niit.bej.master.challenge.service;
+
+import com.niit.bej.master.challenge.domain.Product;
+
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getAllProduct() {
+        return null;
+    }
+}
