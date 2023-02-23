39279caf9f6bd4ba35d67a57a779eeb8f7be6652
yashwantmhamane <yashwantmhamane@gmail.com>
2023-02-23 16:31:46 +0530

    Initial Commit

---
+++ src/main/java/com/niit/bej/master/challenge/MasterChallengeSprint05Application.java
@@ -0,0 +1,13 @@
+package com.niit.bej.master.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterChallengeSprint05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterChallengeSprint05Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/niit/bej/master/challenge/MasterChallengeSprint05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.bej.master.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterChallengeSprint05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
