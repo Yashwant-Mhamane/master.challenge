a5fb0299b0aabe3600abe926ed676c89229eb5e4
yashwantmhamane <yashwantmhamane@gmail.com>
2023-02-23 17:01:05 +0530

    @Autowired ProductRepository and returned

--- src/main/java/com/niit/bej/master/challenge/service/ProductServiceImpl.java
+++ src/main/java/com/niit/bej/master/challenge/service/ProductServiceImpl.java
@@ -1,19 +1,28 @@
 package com.niit.bej.master.challenge.service;
 
 import com.niit.bej.master.challenge.domain.Product;
+import com.niit.bej.master.challenge.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @Override
     public Product addProduct(Product product) {
-        return null;
+        return productRepository.save(product);
     }
 
     @Override
     public List<Product> getAllProduct() {
-        return null;
+        return (List<Product>) productRepository.findAll();
     }
 }
