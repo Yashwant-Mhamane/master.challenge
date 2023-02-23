bdd4b5484c97cfe1d0dda27521f32ee80fcbd656
yashwantmhamane <yashwantmhamane@gmail.com>
2023-02-23 17:12:20 +0530

    get data method created and @getmapping annotation done

--- src/main/java/com/niit/bej/master/challenge/controller/ProductController.java
+++ src/main/java/com/niit/bej/master/challenge/controller/ProductController.java
@@ -1,8 +1,12 @@
 package com.niit.bej.master.challenge.controller;
 
+import com.niit.bej.master.challenge.domain.Product;
 import com.niit.bej.master.challenge.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -12,4 +16,10 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
+
+    @GetMapping
+    public List<Product> getAllProduct() {
+        return productService.getAllProduct();
+    }
+
 }
