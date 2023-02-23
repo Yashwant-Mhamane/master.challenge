eba89f685b6478537396fac979958893496ef850
yashwantmhamane <yashwantmhamane@gmail.com>
2023-02-23 16:52:44 +0530

    @Service annotation added

--- src/main/java/com/niit/bej/master/challenge/service/ProductServiceImpl.java
+++ src/main/java/com/niit/bej/master/challenge/service/ProductServiceImpl.java
@@ -1,9 +1,11 @@
 package com.niit.bej.master.challenge.service;
 
 import com.niit.bej.master.challenge.domain.Product;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProductServiceImpl implements ProductService {
     @Override
     public Product addProduct(Product product) {
