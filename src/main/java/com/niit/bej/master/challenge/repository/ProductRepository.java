d89db9316d664ec7062ffde1fae1a71f6e79ecfc
yashwantmhamane <yashwantmhamane@gmail.com>
2023-02-23 16:56:44 +0530

    @Repository annotation added

--- src/main/java/com/niit/bej/master/challenge/repository/ProductRepository.java
+++ src/main/java/com/niit/bej/master/challenge/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.niit.bej.master.challenge.repository;
 
 import com.niit.bej.master.challenge.domain.Product;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
 }
