44c25390a9518bf4aa3d17dee31587f540a51b8e
yashwantmhamane <yashwantmhamane@gmail.com>
2023-02-23 16:39:59 +0530

    Getters and setter added to Product.java

--- src/main/java/com/niit/bej/master/challenge/domain/Product.java
+++ src/main/java/com/niit/bej/master/challenge/domain/Product.java
@@ -19,4 +19,27 @@ public class Product {
     public Product() {
     }
 
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
 }
