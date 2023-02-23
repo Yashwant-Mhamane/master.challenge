674834c49d2cf42cef5a0bd293871dfedec604ce
yashwantmhamane <yashwantmhamane@gmail.com>
2023-02-23 16:40:37 +0530

    equals and hashcode added to Product.java

--- src/main/java/com/niit/bej/master/challenge/domain/Product.java
+++ src/main/java/com/niit/bej/master/challenge/domain/Product.java
@@ -3,6 +3,8 @@ package com.niit.bej.master.challenge.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+import java.util.Objects;
+
 @Entity
 public class Product {
     @Id
@@ -41,5 +43,18 @@ public class Product {
 
     public void setManufacturer(String manufacturer) {
         this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return productId == product.productId && Objects.equals(productName, product.productName) && Objects.equals(manufacturer, product.manufacturer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, productName, manufacturer);
     }
 }
