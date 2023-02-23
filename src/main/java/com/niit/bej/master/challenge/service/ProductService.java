127d6e93266e7cc93f7d5885cc81a8ffca1c97c8
yashwantmhamane <yashwantmhamane@gmail.com>
2023-02-23 16:50:48 +0530

    addProduct method added

--- src/main/java/com/niit/bej/master/challenge/service/ProductService.java
+++ src/main/java/com/niit/bej/master/challenge/service/ProductService.java
@@ -1,4 +1,10 @@
 package com.niit.bej.master.challenge.service;
 
+import com.niit.bej.master.challenge.domain.Product;
+
+import java.util.List;
+
 public interface ProductService {
+
+    Product addProduct(Product product);
 }
