476f76befefcb59861e035d10491aaed7dbfbb1e
yashwantmhamane <yashwantmhamane@gmail.com>
2023-02-23 17:10:12 +0530

    @Autowired and ProductService injected

--- src/main/java/com/niit/bej/master/challenge/controller/ProductController.java
+++ src/main/java/com/niit/bej/master/challenge/controller/ProductController.java
@@ -1,7 +1,15 @@
 package com.niit.bej.master.challenge.controller;
 
+import com.niit.bej.master.challenge.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 }
