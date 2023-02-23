3e90d6fee3461a69bab82599184ca897e06eb8e8
yashwantmhamane <yashwantmhamane@gmail.com>
2023-02-23 16:37:51 +0530

    fields created to Product.java class

--- src/main/java/com/niit/bej/master/challenge/domain/Product.java
+++ src/main/java/com/niit/bej/master/challenge/domain/Product.java
@@ -1,7 +1,15 @@
 package com.niit.bej.master.challenge.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Product {
+
+    private long productId;
+    private String productName;
+    private String manufacturer;
+
+
+
 }
